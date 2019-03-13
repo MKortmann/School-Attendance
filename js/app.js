5c79875d7759e1d3df43d01bce4d6cb293aab8be
Dell_MVMS <companymvms@gmail.com>
2019-03-13 21:44:56 +0100

    add: comments

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@
             attendance = {};
 
         nameColumns.each(function() {
-          debugger
             var name = this.innerText;
+            /**Adding properties using square brackets*/
             attendance[name] = [];
 
             for (var i = 0; i <= 11; i++) {
@@ -30,21 +30,31 @@
                 console.log(attendance[name]);
             }
         });
-
+        /**A common use of JSON is to exchange data to/from a web server.
+        When sending data to a web server, the data has to be a string.
+        Convert a JavaScript object into a string with JSON.stringify().*/
         localStorage.attendance = JSON.stringify(attendance);
+        console.log(localStorage.attendance);
     }
 }());
 
 
 /* STUDENT APPLICATION */
 $(function() {
+  /**extract the values of the local storage*/
     var attendance = JSON.parse(localStorage.attendance),
+    /**Extract the values of the missed column*/
         $allMissed = $('tbody .missed-col'),
+    /**Extract the values of all the check input boxes from 0 until 59*/
         $allCheckboxes = $('tbody input');
 
     // Count a student's missed days
     function countMissing() {
         $allMissed.each(function() {
+          /**The parent() is an inbuilt method in jQuery which is used to find
+          the parent element related to the selected element.*/
+          /**The select element is the missedColumn. The parent "tr" inspect
+          is <tr class="student">*/
             var studentRow = $(this).parent('tr'),
                 dayChecks = $(studentRow).children('td').children('input'),
                 numMissed = 0;
@@ -60,11 +70,21 @@ $(function() {
     }
 
     // Check boxes, based on attendace records
+    /**$.each is A generic iterator function, which can be used to seamlessly
+    iterate over both objects and arrays.
+    jQuery.each( array, callback )
+    array: The array or array-like object to iterate over.
+    callback: The function that will be executed on every value.*/
     $.each(attendance, function(name, days) {
         var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
             dayChecks = $(studentRow).children('.attend-col').children('input');
 
         dayChecks.each(function(i) {
+        /**.prop()
+Categories: Attributes | Manipulation > General Attributes
+Get the value of a property for the first element in the set of matched elements or
+set one or more properties for every matched element.*/
+/**if days[i] is true, it will check the checkbox*/
             $(this).prop('checked', days[i]);
         });
     });
@@ -75,17 +95,27 @@ $(function() {
             newAttendance = {};
 
         studentRows.each(function() {
+          /**get the first name of this line, ex.: Slappy the Frog*/
             var name = $(this).children('.name-col').text(),
+              /**get the checkboxes of this line*/
                 $allCheckboxes = $(this).children('td').children('input');
-
+            /**add new array with the line name*/
             newAttendance[name] = [];
 
             $allCheckboxes.each(function() {
+              /**here $(this) is exactly the checkbox*/
+              /**$(this).prop("checked"): it returns true if it is checked and
+              false if is not checked*/
+              /**So, this newAttendance object, with be filled with many arrays.
+              Each array willl have the name of the line and the values if it
+              is checked or not.*/
                 newAttendance[name].push($(this).prop('checked'));
             });
         });
 
+        /**Update the count*/
         countMissing();
+        /**update the local storage*/
         localStorage.attendance = JSON.stringify(newAttendance);
     });
 
